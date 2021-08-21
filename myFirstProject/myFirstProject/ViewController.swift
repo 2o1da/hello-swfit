3a872bebde110a60255e7050e081596d4c1ae3a4
Solda <solda@khu.ac.kr>
2021-08-21 23:16:33 +0900

    Initialize my first project

---
+++ myFirstProject/myFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstProject
+//
+//  Created by SOLDA on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
